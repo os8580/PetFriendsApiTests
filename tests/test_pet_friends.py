65ac9a3ec1d796cf5dc686fa2a7364e0f0acc3f7
Oleg Sushko <kindovso@gmail.com>
2023-07-30 12:58:01 +0200

    misprint

--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -9,7 +9,7 @@ pf = PetFriens()
 
 # Ниже 5 позитивных тестов
 def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
-    """ Проверяем что запрос api ключа возвращает статус 200 и в тезультате содержится слово key"""
+    """ Проверяем что запрос api ключа возвращает статус 200 и в результате содержится слово key"""
 
     # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
     status, result = pf.get_api_key(email, password)
